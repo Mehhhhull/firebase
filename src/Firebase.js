d1755e3a330b40900c83287e3d09f0ee4a56bd4a
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-08-15 00:02:30 +0530

    ...

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYXuZzFMp8JPOzzas41lClRAYyd29f-Uc",
+  authDomain: "fir-learn-f1fcf.firebaseapp.com",
+  projectId: "fir-learn-f1fcf",
+  storageBucket: "fir-learn-f1fcf.firebasestorage.app",
+  messagingSenderId: "372220248591",
+  appId: "1:372220248591:web:ba58242708f85b99a0197a",
+  measurementId: "G-5HJ8TKDZ96"
+};
+
+
+export const app = initializeApp(firebaseConfig);
